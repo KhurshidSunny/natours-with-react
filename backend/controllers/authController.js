ee3389a3c1897c3bf86094734079f5707705875b
Khurshid Khan <khurshidsunny45@gmail.com>
2024-08-28 11:11:38 +0500

    Make Secure: Data sanitization, limit requests from same api, set Security http headers, and prevent paramter pollution

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,6 +14,19 @@ const signToken = (id) =>
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
@@ -25,14 +38,6 @@ const createSendToken = (user, statusCode, res) => {
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
-  // const newUser = await User.create({
-  //   name: req.body.name,
-  //   email: req.body.email,
-  //   passwordChangedAt: req.body.passwordChangedAt,
-  //   password: req.body.password,
-  //   passwordConfirm: req.body.passwordConfirm,
-  //   role: req.body.role,
-  // });
 
   createSendToken(newUser, 201, res);
 });
